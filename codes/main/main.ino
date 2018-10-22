d50f98d7ec1fe535cd39683c425f6362e8530fba
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2018-10-22 21:29:42 +0100

    main

--- codes/main/main.ino
+++ codes/main/main.ino
@@ -28,16 +28,16 @@ void setup()
     pinMode(PIR_MOTION_SENSOR, INPUT);
     Serial.begin(9600); // Starts the serial communication
 
-    Serial.println("Setting up file on SD card");
-    setupFile(distanceFile, "DIS00.TXT");
-    setupFile(pirFile, "PIR00.TXT");
-    setupFile(loudnessFile, "LOU00.TXT");
-    setupFile(vibrationFile, "VIB00.TXT");
+    // Serial.println("Setting up file on SD card");
+    setupFile(distanceFile, "DIS000.TXT");
+    setupFile(pirFile, "PIR000.TXT");
+    setupFile(loudnessFile, "LOU000.TXT");
+    setupFile(vibrationFile, "VIB000.TXT");
 
     SoftSerial.begin(9600); // the SoftSerial baud rate
 }
 
-void setupFile(File file, String fileName)
+void setupFile(File file, const char *fileName)
 {
     pinMode(10, OUTPUT);
 
@@ -47,20 +47,22 @@ void setupFile(File file, String fileName)
         Serial.println("Card init. failed!");
     }
 
-    char distanceFileName[15];
-    strcpy(distanceFileName, fileName);
+    char tempFileName[15];
+    strcpy(tempFileName, fileName);
+
     for (uint8_t i = 0; i < 100; i++)
     {
-        distanceFileName[6] = '0' + i / 10;
-        distanceFileName[7] = '0' + i % 10;
+        tempFileName[3] = '0' + i / 100;
+        tempFileName[4] = '0' + (i % 10) / 10;
+        tempFileName[5] = '0' + (i % 100) % 10;
 
         // create if does not exist, do not open existing, write, sync after write
-        if (!SD.exists(distanceFileName))
+        if (!SD.exists(tempFileName))
         {
             break;
         }
     }
-    file = SD.open(distanceFileName, FILE_WRITE);
+    file = SD.open(tempFileName, FILE_WRITE);
     if (!file)
     {
         Serial.print("FAILED TO CREATE ");
@@ -70,7 +72,7 @@ void setupFile(File file, String fileName)
         Serial.print("Writing to ");
     }
 
-    Serial.println(distanceFileName);
+    Serial.println(tempFileName);
 }
 
 void loop()
@@ -78,7 +80,7 @@ void loop()
     loudness = analogRead(LOUDNESS);
 
     vibrationData = digitalRead(VIBRATION);
-    Serial.println(vibrationData);
+    // Serial.println(vibrationData);
 
     // Clears the TRIG_PIN
     digitalWrite(TRIG_PIN, LOW);
@@ -96,8 +98,8 @@ void loop()
     distance = duration * 0.034 / 2;
 
     // Prints the distance on the Serial Monitor
-    Serial.print("Distance: ");
-    Serial.println(distance);
+    // Serial.print("Distance: ");
+    // Serial.println(distance);
 
     // NEED TO BE CONSIDERED
     if (digitalRead(PIR_MOTION_SENSOR))
@@ -105,17 +107,20 @@ void loop()
     //if it detects the moving people?
     else
         pirData = "Watching";
-    Serial.println(pirData);
+    // Serial.println(pirData);
 
-    delay(200);
+    delay(500);
 
     // Write the data to the SD card
     outData(distanceFile, distance);
+    delay(500);
     outData(pirFile, pirData);
+    delay(500);
     outData(loudnessFile, loudness);
+    delay(500);
     outData(vibrationFile, vibrationData)
 
-    // delay(500);
+    delay(500);
 }
 
 //
@@ -131,6 +136,6 @@ void outData(File file, String data)
         // print to the serial port too:
         Serial.println(data);
     }
-
+    Serial.println(!file);
     file.flush();
 }
