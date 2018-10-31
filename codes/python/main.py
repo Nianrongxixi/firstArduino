ceb709a4b51a33ba75f2e9ef31d37b6bb51e9a40
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2018-10-31 23:41:15 +0000

    Fourier Transform

--- codes/python/main.py
+++ codes/python/main.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('../../data/LOG000.csv')
-print(df.describe())
-print(df.head())
+# print(df.describe())
+# print(df.head())
 df.replace({'People is coming': 1}, inplace=True)
 df.replace({'Watching': 0}, inplace = True)
-print(df.head())
-distance_50 = df.loc[df['distance'] < 50]
-print(distance_50.describe())
-sns.lineplot(data=distance_50['distance'])
-plt.show()
+# print(df.head())
 
+# sns.lineplot(data=df['loudness'][:9000])
+# plt.show()
+# sns.lineplot(range(14999), abs(np.fft.fft(df[:30000]['loudness']))[1:15000])
+# plt.show()
+# sns.lineplot(range(14999), abs(np.fft.fft(df[38000:68000]['loudness']))[1:15000])
+# plt.show()
+# sns.lineplot(range(14999), abs(np.fft.fft(df[60000:90000]['loudness']))[1:15000])
+# plt.show()
+for i in range(0,len(df),1000):
+    sns.lineplot(range(499), abs(np.fft.fft(df[i:i+1000]['loudness']))[1:500])
+    plt.show()
